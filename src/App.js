53570e727069ff3636b196e7b64f279735186327
Nicholas Radtke <Raddnc@gmail.com>
2021-03-01 13:14:15 -0600

    setting up architecture to project. breaking down projects into components working on row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './App.css';
+import Row from "./Row";
 
 function App() {
   return (
     <div className="App">
-  
+      <Row title="Netflix Originals"/>
+      <Row title="Trending Now"/>
+      <Row title="Top Rated"/>
+      <Row title="Action Movies"/>
+
+
     </div>
   );
 }
