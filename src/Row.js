0b2a4b56baa66f8a7c37b516c85f075bbc064d5b
Nicholas Radtke <Raddnc@gmail.com>
2021-03-01 19:20:09 -0600

    added functionality to row.js

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "./axios";
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 
+function Row({title, fetchUrl, isLargeRow}) {
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
-function Row({title}) {
-
+    useEffect(() => {
+        async function fetchData() {
+          const request = await axios.get(fetchUrl);
+          setMovies(request.data.results); 
+          return request; 
+        }
+        fetchData();
+      }, [fetchUrl]);
 
     return (
        <div className="row">
