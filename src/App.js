b567c7b360d210c67f7186981eba8ebaf9b59530
Nicholas Radtke <Raddnc@gmail.com>
2021-03-01 19:35:08 -0600

    imported nav and banner to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import './App.css';
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 
 function App() {
   return (
     <div className="App">
-      {/* navbar */}
-      {/* banner */}
+      <Nav />
+      <Banner/>
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
