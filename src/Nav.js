96ff7fc0172d5343f726cf65649ce885defde739
Nicholas Radtke <Raddnc@gmail.com>
2021-03-01 20:47:00 -0600

    finished adding logic to nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -4,24 +4,32 @@ import "./Nav.css";
 function Nav() {
     const [show, handleShow] = useState(false);
     useEffect(() => {
-        window.addEventListener("scroll", () => {
+      window.addEventListener("scroll", () => {
         if (window.scrollY > 100) {
-            handleShow(true);
+          handleShow(true);
         } else {
-            handleShow(false);
+          handleShow(false);
         }
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
+      });
+      return () => {
+        window.removeEventListener("scroll");
+      };
     }, []);
-
+  
     return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img src="https://pmcvariety.files.wordpress.com/2020/05/netflix-logo.png?w=300" alt="Netflix Logo" className="nav__logo"/>
-            
-        </div>
-    )
-}
-
-export default Nav
+      <div className={`nav ${show && "nav__black"}`}>
+        <img
+          className="nav__logo"
+          src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+          alt="Netflix Logo"
+        />
+        <img
+          className="nav__avatar"
+          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+          alt="Netflix Avatar"
+        />
+      </div>
+    );
+  }
+  
+  export default Nav;
