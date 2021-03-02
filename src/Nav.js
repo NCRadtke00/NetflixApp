39bfcc43e4ca1c592a83993e3bcbfaf1781e4b70
Nicholas Radtke <Raddnc@gmail.com>
2021-03-01 20:29:40 -0600

    added functionality to nav-bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffects, useEffect} from "react";
 import "./Nav.css";
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
 
     return (
-        <div className="">
+        <div className={`nav ${show && "nav__black"}`}>
             <img src="https://pmcvariety.files.wordpress.com/2020/05/netflix-logo.png?w=300" alt="Netflix Logo" className="nav__logo"/>
             
         </div>
