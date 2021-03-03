71c5d94e76cd2599e31a9d92b6ced2c2be523ad3
Nicholas Radtke <Raddnc@gmail.com>
2021-03-03 01:25:32 -0600

    added function to navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,38 +1,40 @@
-import React, { useState, useEffect} from "react";
-import {useHistory} from 'react-router-dom';
-import "./Nav.css";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import './Nav.css';
 
 function Nav() {
     const [show, handleShow] = useState(false);
     const history = useHistory();
 
     const transitionNavBar = () => {
-      if (window.scrollY > 100) {
-        handleShow(true);
-      }else{
-        handleShow(false);
-      }
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
     }
     useEffect(() => {
-      window.addEventListener("scroll", transitionNavBar);
-      return () => window.addEventListener("scroll", transitionNavBar);
-    }, []);
-  
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.addEventListener("scroll", transitionNavBar);
+    }, [])
     return (
-      <div className={`nav ${show && "nav__black"}`}>
-        <img
-          onClick={()=> history.push("/")}
-          className="nav__logo"
-          src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
-          alt="Netflix Logo"
-        />
-        <img
-          className="nav__avatar"
-          src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
-          alt="Netflix Avatar"
-        />
-      </div>
-    );
-  }
-  
-  export default Nav;
+        <div className={`nav ${show && 'nav__black'}`}>
+            <div className="nav__contents">
+                <img
+                    onClick={() => history.push("/")}
+                    className="nav__logo"
+                    src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.png"
+                    alt="netflix-logo"
+                />
+                <img
+                    onClick={() => history.push("/profile")}
+                    className="nav__avatar"
+                    src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+                    alt="netflix-avatar"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
